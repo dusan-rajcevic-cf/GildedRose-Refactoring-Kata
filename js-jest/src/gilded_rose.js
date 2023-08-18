e290a75d34a26fe29004ae2157de9b5a7ad4f602
Dusan Rajcevic <dusan@awardforce.com>
2023-08-18 11:16:43 +0200

    Extracted the branch from for loop into a new method.

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -12,67 +12,70 @@ class Shop {
   }
   updateQuality() {
     for (let item of this.items) {
+      this.update(item);
+    }
+
+    return this.items;
+  }
+
+  update(item) {
+    if (this.isConjured(item)) {
+      if (item.quality - 2 >= 0) {
+        item.quality = item.quality - 2;
+      } else {
+        item.quality = 0;
+      }
+    }
+    if (!this.isAgedBrie(item) && !this.isBackstage(item) && !this.isConjured(item)) {
+      if (item.quality > 0) {
+        if (!this.isSulfuras(item)) {
+          item.quality = item.quality - 1;
+        }
+      }
+    } else if (!this.isConjured(item)) {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+        if (this.isBackstage(item)) {
+          if (item.sellIn < 11) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
+            }
+          }
+          if (item.sellIn < 6) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
+            }
+          }
+        }
+      }
+    }
+    if (!this.isSulfuras(item)) {
+      item.sellIn = item.sellIn - 1;
+    }
+    if (item.sellIn < 0) {
       if (this.isConjured(item)) {
         if (item.quality - 2 >= 0) {
           item.quality = item.quality - 2;
         } else {
           item.quality = 0;
         }
-      }
-      if (!this.isAgedBrie(item) && !this.isBackstage(item) && !this.isConjured(item)) {
-        if (item.quality > 0) {
-          if (!this.isSulfuras(item)) {
-            item.quality = item.quality - 1;
-          }
-        }
-      } else if (!this.isConjured(item)) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-          if (this.isBackstage(item)) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
+      } else if (!this.isAgedBrie(item)) {
+        if (!this.isBackstage(item)) {
+          if (item.quality > 0) {
+            if (!this.isSulfuras(item)) {
+              item.quality = item.quality - 1;
             }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (!this.isSulfuras(item)) {
-        item.sellIn = item.sellIn - 1;
-      }
-      if (item.sellIn < 0) {
-        if (this.isConjured(item)) {
-          if (item.quality - 2 >= 0) {
-            item.quality = item.quality - 2;
-          } else {
-            item.quality = 0;
-          }
-        } else if (!this.isAgedBrie(item)) {
-          if (!this.isBackstage(item)) {
-            if (item.quality > 0) {
-              if (!this.isSulfuras(item)) {
-                item.quality = item.quality - 1;
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality;
           }
         } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
+          item.quality = item.quality - item.quality;
+        }
+      } else {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
         }
       }
     }
-
-    return this.items;
   }
-
   isAgedBrie(item) {
     return item.name === 'Aged Brie';
   }
