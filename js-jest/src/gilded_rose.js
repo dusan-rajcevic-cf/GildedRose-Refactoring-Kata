823c870aac12984c3f79bba9cf91014f170617a2
Dusan Rajcevic <dusan@awardforce.com>
2023-08-18 11:07:57 +0200

    Added Conjured Item to the Shop.

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -12,13 +12,20 @@ class Shop {
   }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.items[i].name == 'Conjured Mana Cake') {
+        if (this.items[i].quality - 2 >= 0) {
+          this.items[i].quality = this.items[i].quality - 2;
+        } else {
+          this.items[i].quality = 0;
+        }
+      }
+      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Conjured Mana Cake') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
-      } else {
+      } else if (this.items[i].name != 'Conjured Mana Cake') {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
@@ -39,7 +46,13 @@ class Shop {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
+        if (this.items[i].name == 'Conjured Mana Cake') {
+          if (this.items[i].quality - 2 >= 0) {
+            this.items[i].quality = this.items[i].quality - 2;
+          } else {
+            this.items[i].quality = 0;
+          }
+        } else if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
